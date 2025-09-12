cd07f57af7db7f4b26b4885e3c8bb67aec607cb7
SofiaCarolini <00001108952008sp@al.educacao.sp.gov.br>
2025-09-12 11:36:34 -0300

    Add files via upload

---
+++ README.MD
@@ -0,0 +1,14 @@
+# Site acessível sobre Tim Maia
+# Sobre
+Refatoração de um site implementando recursos recursos de acessibilidade no html, css e JS.
+## Recursos de acessibilidade
+- Atributo aria
+- Alt
+- tab-index
+- menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollRevealjs
+- HTML
+- CSS
+- JS
--- script.js
+++ script.js
@@ -5,10 +5,15 @@ document.addEventListener("DOMContentLoaded", function(){
      botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
      })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
   
      let tamanhoAtualFonte = 1;
   
@@ -23,5 +28,14 @@ document.addEventListener("DOMContentLoaded", function(){
       document.body.style.fontSize = '${tamanhoAtualFonte}rem'
      })
   
+     alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+     })
   
   })
+
+  ScrollReveal().reveal('#inicio', { delay: 500 });
+  ScrollReveal().reveal('#tropicalia', { delay: 500 });
+  ScrollReveal().reveal('#galeria', { delay: 500 });
+  ScrollReveal().reveal('#contato', { delay: 500 });
+
